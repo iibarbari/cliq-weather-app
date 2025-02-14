88913921d782f62a79dc874c6b97dad01446317f
iibarbari <45620986+iibarbari@users.noreply.github.com>
2025-02-14 17:57:26 +0300

    add storybook

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import { JSX, PropsWithoutRef } from 'react';
+
+type Props = PropsWithoutRef<JSX.IntrinsicElements["button"]>
+
+export default function Button({ ...props }: Props) {
+  return (
+    <button {...props} />
+  );
+}
